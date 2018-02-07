5098229fa9db6dde1d17dea6b2fe81e8c7ddc525
Daniel Green <KasumiL5x@gmail.com>
2018-02-07 13:40:04 +0000

    Initial Commit

---
+++ imgcmp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  imgcmp
+//
+//  Created by Daniel Green on 07/02/2018.
+//  Copyright © 2018 Daniel Green. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
